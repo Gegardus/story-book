23d4e62a6dcb66ca2ed0875cf381a17e0e035faa
Gegardus <stgegard@gmail.com>
2022-11-30 18:05:41 +0400

    add turbo-stream

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -28,6 +28,7 @@ class StoriesController < ApplicationController
     respond_to do |format|
       if @story.update(story_params)
         format.html { redirect_to stories_path }
+        # Here, without refreshing the website, the user interface will reflect with those stories which are updated into the database
         format.turbo_stream { render turbo_stream: turbo_stream.replace("story_#{@story.id}", partial: 'story', locals: { story: @story }) }
       else
         format.html { redirect_to edit_story_path }
@@ -39,6 +40,9 @@ class StoriesController < ApplicationController
     respond_to do |format|
       if @story.destroy
         format.html { redirect_to stories_path }
+        # using turbo_frame helper, the request is sent as turbo_stream format when submitting a form. 
+        # Therefore, responses must be sent back in the same turbo_stream format. 
+        # Therefore, we created a turbo_stream.erb file using the response_to block in this
         format.turbo_stream
       else
         format.html { redirect_to stories_path, notice: @story.errors.full_message }
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,9 @@
 class Story < ApplicationRecord
   validates :title, :description, presence: true
+  # syntax for broadcasting data 
+  # Here, the newly created stories are broadcast to the stories channel 
+  # using a callback that is triggered each time a new story is created. 
+  # This type of streaming requires the setup of a WebSocket connection 
+  # to the channel that will receive the updates
   after_create_commit { broadcast_prepend_to 'stories' }
 end
